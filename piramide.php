3590b1911fce85269e416754d019e1e01ee00f0c
et4321 <54795466+et4321@users.noreply.github.com>
2019-10-10 09:24:25 +0200

    Uitwerking van de exercise

---
+++ piramide.php
@@ -0,0 +1,18 @@
+<?php
+
+echo "hoe groot wil je de pirmaide maken?\n";
+$grooten = readline();
+
+for ($i =1; $i <= $grooten; $i++){
+
+    for($o =1; $o <= $i; $o++){
+         echo "*";
+        }
+
+        echo "\n";
+        
+
+    }
+
+
+?>
